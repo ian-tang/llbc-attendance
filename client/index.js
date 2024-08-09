f2d611b62b9d985790f5ad314954adba0c65ac0e
Ian Tang <ian.tang@protonmail.com>
2024-08-09 17:43:00 -0400

    feat: sync value of checkbox in modal and checkbox on main form

--- client/index.js
+++ client/index.js
@@ -2,18 +2,27 @@
 
 document.onreadystatechange = () => {
   if (document.readyState === 'interactive') {
-    const waiverButton = document.getElementById('liability-terms-btn')
+    const showWaiverButton = document.getElementById('liability-terms-btn')
     const overlay = document.getElementById('modal-overlay')
     const modal = document.getElementById('modal')
+
+    const modalCheckbox = document.getElementById('modal-checkbox')
+    const mainFormCheckbox = document.getElementById('liability-waiver')
+
+    const signInForm = document.getElementById('sign-in')
 
     const hideModal = () => {
       overlay.className = 'hidden'
       modal.className = 'hidden'
+
+      mainFormCheckbox.checked = modalCheckbox.checked
     }
 
     const showModal = () => {
       overlay.className = ''
       modal.className = ''
+
+      modalCheckbox.checked = mainFormCheckbox.checked
     }
 
     document.addEventListener('keydown', (event) => {
@@ -24,6 +33,6 @@ document.onreadystatechange = () => {
 
     overlay.addEventListener('click', hideModal)
 
-    waiverButton.addEventListener('click', showModal)
+    showWaiverButton.addEventListener('click', showModal)
   }
 }
