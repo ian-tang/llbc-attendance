430f143921b83a671212f7600524d4c8a4e2f0df
Ian Tang <ian.tang@protonmail.com>
2024-08-08 23:05:13 -0400

    feat: add basic modal structure for liability form

--- client/index.js
+++ client/index.js
@@ -1,1 +1,29 @@
 'use strict'
+
+document.onreadystatechange = () => {
+  if (document.readyState === 'interactive') {
+    const waiverButton = document.getElementById('liability-terms-btn')
+    const overlay = document.getElementById('modal-overlay')
+    const modal = document.getElementById('modal')
+
+    const hideModal = () => {
+      overlay.className = 'hidden'
+      modal.className = 'hidden'
+    }
+
+    const showModal = () => {
+      overlay.className = ''
+      modal.className = ''
+    }
+
+    document.addEventListener('keydown', (event) => {
+      if (event instanceof KeyboardEvent && event.key === 'Escape') {
+        hideModal()
+      }
+    })
+
+    overlay.addEventListener('click', hideModal)
+
+    waiverButton.addEventListener('click', showModal)
+  }
+}
