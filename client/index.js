ce211d3343f5cadb6b1920945f7d1c9d86c22e2b
Ian Tang <ian.tang@protonmail.com>
2024-08-10 01:36:31 -0400

    feat: add basic form validation through js

--- client/index.js
+++ client/index.js
@@ -1,5 +1,67 @@
 'use strict'
 
+const VALID_ENTRY = {
+  valid: true,
+  message: '',
+}
+
+const VALIDATION_METHODS = {
+  'first-name': (value) => {
+    if (!value.length)
+      return {
+        valid: false,
+        message: 'This field is required',
+      }
+    if (value.search(/[0-9]/) > -1)
+      return {
+        valid: false,
+        message: 'Name cannot contain numbers',
+      }
+
+    return VALID_ENTRY
+  },
+
+  'last-name': (value) => {
+    if (!value.length)
+      return {
+        valid: false,
+        message: 'This field is required',
+      }
+    if (value.search(/[0-9]/) > -1)
+      return {
+        valid: false,
+        message: 'Name cannot contain numbers',
+      }
+
+    return VALID_ENTRY
+  },
+
+  'phone-number': (value) => {
+    if (value.length === 0)
+      return {
+        valid: false,
+        message: 'This field is required',
+      }
+    if (value.search(/[\D]/) > -1)
+      return {
+        valid: false,
+        message: 'Phone number can only contain numbers',
+      }
+
+    return VALID_ENTRY
+  },
+
+  'liability-waiver': (value) => {
+    if (value !== 'true')
+      return {
+        valid: false,
+        message: 'Liability waiver is required',
+      }
+
+    return VALID_ENTRY
+  },
+}
+
 document.onreadystatechange = () => {
   if (document.readyState === 'interactive') {
     const showWaiverButton = document.getElementById('liability-terms-btn')
@@ -22,6 +84,7 @@ document.onreadystatechange = () => {
       overlay.className = ''
       modal.className = ''
 
+      modal.focus()
       modalCheckbox.checked = mainFormCheckbox.checked
     }
 
@@ -34,5 +97,54 @@ document.onreadystatechange = () => {
     overlay.addEventListener('click', hideModal)
 
     showWaiverButton.addEventListener('click', showModal)
+
+    document.getElementById('submit-btn').addEventListener('click', (event) => {
+      event.preventDefault()
+      const formFields = {
+        'first-name': undefined,
+        'last-name': undefined,
+        'phone-number': undefined,
+        'liability-waiver': undefined,
+      }
+      const data = new FormData(signInForm)
+      let isFormValid = true
+
+      for (const [name, value] of data) {
+        formFields[name] = value
+      }
+      for (const [name, value] of Object.entries(formFields)) {
+        const validationResult = VALIDATION_METHODS[name]?.(value)
+        if (validationResult && !validationResult.valid) {
+          const el = document.getElementById(name)
+
+          // scrolls into view only for 1st invalid field
+          if (isFormValid) el.scrollIntoView({ behavior: 'smooth' })
+          isFormValid = false
+
+          el.classList.add('invalid-field-warning')
+          if (el.nextElementSibling.classList.contains('error-message')) {
+            el.nextElementSibling.classList.remove('hidden')
+            el.nextElementSibling.innerHTML = validationResult.message
+          }
+
+          el.addEventListener('input', () => {
+            if (VALIDATION_METHODS[name](el.value).valid === true) {
+              el.classList.remove('invalid-field-warning')
+              if (el.nextElementSibling.classList.contains('error-message')) {
+                el.nextElementSibling.classList.add('hidden')
+              }
+            }
+          })
+        }
+      }
+
+      console.log('form is complete:', isFormValid)
+
+      if (isFormValid) signInForm.requestSubmit()
+    })
+
+    signInForm.addEventListener('submit', (event) => {
+      console.log('submitted')
+    })
   }
 }
