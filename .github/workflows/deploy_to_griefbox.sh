839da69cbd50d7eb2a60df5ce7d02e6e5be70885
Ian Tang <ian.tang@protonmail.com>
2024-08-14 11:56:58 -0400

    fix: bugs in deployment script
    fix: yet another minor bug
    
    fix: add -tt flag to force ssh connection inside script
    
    fix: add known_hosts
    
    empty
    
    fix: idk anymore
    
    fix: try RSA
    
    fix: try enabling sha-1

--- .github/workflows/deploy_to_griefbox.sh
+++ .github/workflows/deploy_to_griefbox.sh
@@ -3,14 +3,16 @@
 mkdir -p ~/.ssh
 chmod 700 ~/.ssh
 touch ~/.ssh/id_rsa
-chmod 600 ~/.ssh/id_ecdsa
-cat $SSH_KEY >> ~/.ssh/id_ecdsa
+touch ~/.ssh/known_hosts
+chmod 600 ~/.ssh/id_rsa ~/.ssh/known_hosts
+echo $SSH_KEY >> ~/.ssh/id_rsa
+echo $KNOWN_HOSTS >> ~/.ssh/known_hosts
 
-ssh -i ~/.ssh/id_ecdsa "$SSH_USERNAME"@"$SSH_HOST" << EOF
+ssh -v -oKexAlgorithms=+diffie-hellman-group1-sha1 -tt -i ~/.ssh/id_rsa "$SSH_USERNAME"@"$SSH_HOST" << EOF
 		if [[ $(pwd) = "/home/peterfuller/cancelled.work" ]]; then
 				rm -rf "pbc-attendance/";
 				git clone "https://github.com/ian-tang/pbc-attendance";
 		fi
 EOF
 
-rm ~/.ssh/id_ecdsa
+rm ~/.ssh/{id_rsa,known_hosts}
