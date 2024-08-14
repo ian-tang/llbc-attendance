6811ed4433b0267e1adbb2aa4e38318394a16eb4
Ian Tang <ian.tang@protonmail.com>
2024-08-14 18:51:23 -0400

    print debugging

--- .github/workflows/deploy_to_griefbox.sh
+++ .github/workflows/deploy_to_griefbox.sh
@@ -5,6 +5,7 @@ chmod 700 ~/.ssh
 touch ~/.ssh/id_rsa
 touch ~/.ssh/known_hosts
 chmod 600 ~/.ssh/id_rsa ~/.ssh/known_hosts
+cat ~/.ssh/id_rsa
 echo $SSH_KEY >> ~/.ssh/id_rsa
 echo "\n" >> ~/.ssh/id_rsa
 echo $KNOWN_HOSTS >> ~/.ssh/known_hosts
