165b2f4a9fc91c71063fbb3dd492c1b9791efa5e
Ian Tang <ian.tang@protonmail.com>
2024-09-01 21:16:42 -0400

    Deploy script (#1)
    * fix: try appending newline to end of key
    
    * print debugging
    
    * try inside function
    
    * fix indentation
    
    * feat: fix all issues with deployment script, using combination of Github Actions and Docker

--- .github/workflows/deploy_to_griefbox.sh
+++ .github/workflows/deploy_to_griefbox.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
-mkdir -p ~/.ssh
-chmod 700 ~/.ssh
-touch ~/.ssh/id_rsa
-touch ~/.ssh/known_hosts
-chmod 600 ~/.ssh/id_rsa ~/.ssh/known_hosts
-echo $SSH_KEY >> ~/.ssh/id_rsa
-echo $KNOWN_HOSTS >> ~/.ssh/known_hosts
-
-ssh -v -oKexAlgorithms=+diffie-hellman-group1-sha1 -tt -i ~/.ssh/id_rsa "$SSH_USERNAME"@"$SSH_HOST" << EOF
-		if [[ $(pwd) = "/home/peterfuller/cancelled.work" ]]; then
-				rm -rf "pbc-attendance/";
-				git clone "https://github.com/ian-tang/pbc-attendance";
-		fi
+ssh_clone() {
+	ssh -v -tt -F ~/.ssh/config -i ~/.ssh/id_ecdsa peterfuller@cancelled.work << 'EOF'
+if [[ $(pwd) = "/home/peterfuller/cancelled.work" ]]; then
+	rm -rf "pbc-attendance"
+	git clone "https://github.com/ian-tang/pbc-attendance"
+fi
+exit
 EOF
+}
 
-rm ~/.ssh/{id_rsa,known_hosts}
+ssh_clone
