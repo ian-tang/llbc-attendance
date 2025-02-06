11f06184cd0272ebae57ab2067de598de5913703
Ian Tang <ian.tang@protonmail.com>
2025-02-05 20:16:53 -0500

    feat: add queries for creating tables

--- server/db/db.go
+++ server/db/db.go
@@ -28,7 +28,43 @@ func init() {
 		log.Fatal(err)
 	}
 	db = conn
+	createTables()
 	log.Print("DB initialized")
+}
+
+func createTables() {
+	createUsersTbl := `
+	CREATE TABLE IF NOT EXISTS users (
+		id INTEGER NOT NULL PRIMARY KEY,
+		first_name TEXT NOT NULL,
+		last_name TEXT NOT NULL,
+		email TEXT NOT NULL UNIQUE,
+		newsletter BOOLEAN NOT NULL,
+		photo_release BOOLEAN NOT NULL,
+		creation_time TEXT NOT NULL
+	);
+`
+	createResponsesTbl := `
+	CREATE TABLE IF NOT EXISTS user_responses (
+		id INTEGER NOT NULL PRIMARY KEY,
+		user_id INTEGER NOT NULL,
+		role_volunteer BOOLEAN NOT NULL,
+		role_get_assistance BOOLEAN NOT NULL,
+		role_purchase BOOLEAN NOT NULL,
+		role_donate BOOLEAN NOT NULL,
+		submission_time TEXT NOT NULL,
+		FOREIGN KEY (user_id)
+			REFERENCES users (id)
+	);
+`
+	_, err := db.Exec(createUsersTbl)
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = db.Exec(createResponsesTbl)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func New() *sql.DB {
