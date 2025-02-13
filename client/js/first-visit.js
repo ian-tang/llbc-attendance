fa11431ce745af8b5d264e29af867589e308c3b2
Ian Tang <ian.tang@protonmail.com>
2025-02-13 16:24:45 -0500

    refactor: add liability waiver and photo release language, revise modal-related scripting

--- client/js/first-visit.js
+++ client/js/first-visit.js
@@ -1,14 +1,20 @@
 import { getCurrentFormValues, validateAndSubmit } from './lib.js'
 import { postNewVisitor } from './api.js'
+import { LIABILITY_TEXT } from '../legal/liability_waiver.js'
+import { PHOTO_RELEASE } from '../legal/photo_release.js'
 
 /**
  * @typedef {object} RequiredElements
  * @property {HTMLButtonElement} showWaiverButton
+ * @property {HTMLButtonElement} showPhotoReleaseButton
  * @property {HTMLDivElement} overlay
  * @property {HTMLElement} modal
+ * @property {HTMLHeadingElement} modalTitle
+ * @property {HTMLElement} modalBody
  * @property {HTMLButtonElement} modalCloseButton
  * @property {HTMLInputElement} modalCheckbox
- * @property {HTMLInputElement} mainFormCheckbox
+ * @property {HTMLInputElement} liabilityCheckbox
+ * @property {HTMLInputElement} photoReleaseCheckbox
  * @property {HTMLFormElement} signInForm
  * @property {HTMLButtonElement} formSubmitButton
  */
@@ -33,12 +39,16 @@ document.onreadystatechange = () => {
   ) {
     // check for necessary DOM elements
     const showWaiverButton = document.getElementById('liability-terms-btn')
+    const showPhotoReleaseButton = document.getElementById('photo-release-btn')
     const overlay = document.getElementById('modal-overlay')
     const modal = document.getElementById('modal')
+    const modalTitle = document.getElementById('modal-title')
+    const modalBody = document.getElementById('modal-body')
     const modalCloseButton = document.getElementById('modal-close-btn')
 
     const modalCheckbox = document.getElementById('modal-checkbox')
-    const mainFormCheckbox = document.getElementById('liability-waiver')
+    const liabilityCheckbox = document.getElementById('liability-waiver')
+    const photoReleaseCheckbox = document.getElementById('photo-release')
 
     const signInForm = document.getElementById('sign-in')
     const formSubmitButton = document.getElementById('submit-btn')
@@ -46,11 +56,15 @@ document.onreadystatechange = () => {
     /** @type RequiredElements */
     const requiredElements = {
       showWaiverButton,
+      showPhotoReleaseButton,
       overlay,
       modal,
+      modalTitle,
       modalCloseButton,
       modalCheckbox,
-      mainFormCheckbox,
+      liabilityCheckbox,
+      photoReleaseCheckbox,
+      modalBody,
       signInForm,
       formSubmitButton,
     }
@@ -69,39 +83,75 @@ document.onreadystatechange = () => {
  */
 function attachPageEventListeners({
   showWaiverButton,
+  showPhotoReleaseButton,
   overlay,
   modal,
+  modalTitle,
+  modalBody,
   modalCloseButton,
   modalCheckbox,
-  mainFormCheckbox,
+  liabilityCheckbox,
+  photoReleaseCheckbox,
   signInForm,
   formSubmitButton,
 }) {
   const hideModal = () => {
-    overlay.classList.add('hidden')
-    modal.classList.add('hidden')
-
-    if (modalCheckbox.checked !== mainFormCheckbox.checked)
-      mainFormCheckbox.click()
+    overlay.hidden = true
+    modal.hidden = true
   }
 
   const showModal = () => {
-    overlay.classList.remove('hidden')
-    modal.classList.remove('hidden')
-
-    modal.focus()
-    modalCheckbox.checked = mainFormCheckbox.checked
+    overlay.hidden = false
+    modal.hidden = false
   }
 
-  document.addEventListener('keydown', (event) => {
-    if (event instanceof KeyboardEvent && event.key === 'Escape') {
-      hideModal()
-    }
-  })
+  const displayModal = (title, body, checkboxRef) => {
+    modalCheckbox.checked = checkboxRef.checked
+    modalTitle.innerText = title
+    modalBody.innerHTML = body
 
-  modalCloseButton.addEventListener('click', hideModal)
-  overlay.addEventListener('click', hideModal)
-  showWaiverButton.addEventListener('click', showModal)
+    const mainTag = document.getElementsByTagName('main')[0]
+    const focusableElements = mainTag.querySelectorAll('input, button')
+    focusableElements.forEach((el) => (el.tabIndex = -1))
+
+    const syncCheckbox = () => {
+      checkboxRef.checked = modalCheckbox.checked
+    }
+
+    modalCheckbox.addEventListener('click', syncCheckbox)
+
+    const closeModal = () => {
+      focusableElements.forEach((el) => (el.tabIndex = 0))
+      hideModal()
+      modalCheckbox.removeEventListener('click', syncCheckbox)
+      checkboxRef.focus()
+    }
+
+    overlay.addEventListener('click', closeModal)
+    modalCloseButton.addEventListener('click', closeModal)
+    document.addEventListener('keydown', (event) => {
+      if (event instanceof KeyboardEvent && event.key === 'Escape') {
+        closeModal()
+      }
+    })
+    showModal()
+    modalCheckbox.focus()
+  }
+
+  showWaiverButton.addEventListener('click', () =>
+    displayModal(
+      'Liability Waiver for Lefty Loosey Bicycle Collective',
+      LIABILITY_TEXT,
+      liabilityCheckbox,
+    ),
+  )
+  showPhotoReleaseButton.addEventListener('click', () =>
+    displayModal(
+      'Photo Release for Lefty Loosey Bicycle Collective',
+      PHOTO_RELEASE,
+      photoReleaseCheckbox,
+    ),
+  )
   formSubmitButton.addEventListener('click', (event) =>
     validateAndSubmit(event, signInForm, FORM_FIELDS),
   )
