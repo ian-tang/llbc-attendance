7d53f7faf684cfb52a246e93364a452d69439114
Ian Tang <ian.tang@protonmail.com>
2025-03-01 13:50:50 -0500

    Update deploy_to_griefbox.sh

--- .github/workflows/deploy_to_griefbox.sh
+++ .github/workflows/deploy_to_griefbox.sh
@@ -3,8 +3,8 @@
 ssh_clone() {
 	ssh -v -tt -F ~/.ssh/config -i ~/.ssh/id_ecdsa peterfuller@cancelled.work << 'EOF'
 if [[ $(pwd) = "/home/peterfuller/cancelled.work" ]]; then
-	rm -rf "pbc-attendance"
-	git clone "https://github.com/ian-tang/pbc-attendance"
+	rm -rf "llbc-attendance"
+	git clone "https://github.com/ian-tang/llbc-attendance"
 fi
 exit
 EOF
